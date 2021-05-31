1de700f08d236bccf5571de345845079edcc2eff
Lee Tibbert <lee.tibbert@gmail.com>
2021-05-31 12:25:06 -0400

    Thrashing 2021-05-31 12:25 -0400

--- posixlib/src/main/resources/scala-native/netdb.c
+++ posixlib/src/main/resources/scala-native/netdb.c
@@ -43,6 +43,8 @@ int scalanative_getnameinfo(struct scalanative_sockaddr *addr,
                              servlen, flags);
 }
 
+// #elif defined(no__linux__)
+
 // #elif defined(__linux__)
 
 #elif defined(__APPLE__) || defined(__FreeBSD__)
@@ -68,16 +70,18 @@ void scalanative_convert_scalanative_addrinfo(struct scalanative_addrinfo *in,
                                               struct addrinfo *out) {
     // ai_addr and ai_next fields are set to NULL because this function is only
     // used for converting hints parameter for the getaddrinfo function, which
-    // doesn't care about them
+    // doesn't
+    // care about them
     out->ai_flags = in->ai_flags;
     out->ai_family = in->ai_family;
     out->ai_socktype = in->ai_socktype;
     out->ai_protocol = in->ai_protocol;
     out->ai_addrlen = in->ai_addrlen;
-
-    out->ai_canonname = 
-      (in->ai_canonname == NULL) ? NULL : strdup(in->ai_canonname);
-
+    if (in->ai_canonname == NULL) {
+        out->ai_canonname = NULL;
+    } else {
+        out->ai_canonname = strdup(in->ai_canonname);
+    }
     out->ai_addr = NULL;
     out->ai_next = NULL;
 }
@@ -87,9 +91,6 @@ void scalanative_convert_scalanative_addrinfo(struct scalanative_addrinfo *in,
 
 static void sn_convert_sn_sockaddr_in(
     struct sockaddr_in *in, struct scalanative_sockaddr_in *out) {
-
-  // 2021-05-31 11:36 -0400 LeeT FIXME -- zero only _sin_zero
-    memset(out, 0, sizeof(struct scalanative_sockaddr_in));
     out->sin_port = in->sin_port;
 
     //    sn_convert_sn_in_addr(&(in->sin_addr), &(out->sin_addr));
@@ -101,7 +102,7 @@ static void sn_convert_sn_sockaddr_in(
     // 2021-05-30 15:08 -0400 LeeT FIXME -- make sure there is
     // a/an _Static_assert() in netinet/in.h to enforce that two
     // sizes are the same.
-    // memcpy(out, in, sizeof(struct scalanative_sockaddr_in));
+    memcpy(out, in, sizeof(struct scalanative_sockaddr_in));
 
     out->sin_family = in->sin_family; // also zeros _sa_len, where present.
 }
@@ -109,10 +110,7 @@ static void sn_convert_sn_sockaddr_in(
 // 2021-05-30 20:37 -0400 LeeT FIXME -- this is a quick hack, fix it
 static void sn_convert_sn_sockaddr_in6(
     struct sockaddr_in6 *in, struct scalanative_sockaddr_in6 *out) {
-  printf(",,, sn_convert_sn_sockaddr_in6 memcpy\n");
-    memcpy(out, in, sizeof(struct scalanative_sockaddr_in6));
-
-    out->sin6_family = in->sin6_family; // also zeros _sa_len, where present.
+  printf(",,, sn_convert_sn_sockaddr_in6 UNDER CONSTRUCTION\n");
 }
 
 // 2021-05-30 20:41 -0400 LeeT FIXME -- Delete this forward declaration
@@ -127,29 +125,14 @@ void scalanative_convert_addrinfo_X3(struct addrinfo *in,
     out->ai_family =  in->ai_family;
     out->ai_socktype = in->ai_socktype;
     out->ai_protocol = in->ai_protocol;
-
-      /*
-    if (in->ai_addr == NULL) {
-        out->ai_addr = NULL;
-    } else {
-      struct scalanative_sockaddr *addr =
-	malloc(sizeof(struct scalanative_sockaddr));
-
-      // _Static_asserts in sys/socket.h ensure dst & src sizes match.
-      memcpy(addr, in->ai_addr, sizeof(struct scalanative_sockaddr_in));
-      addr->sa_family = in->ai_family; // also zeros _sa_len, where present.
-
-      out->ai_addr = addr;
-    }
-      */
-
     if (in->ai_addr == NULL) {
         out->ai_addr = NULL;
     } else {
         if (in->ai_addr->sa_family == AF_INET) {
 	  //2021-05-30 14:03 -0400 LeeT fixme -- see if can zero only _sin_zero
             struct scalanative_sockaddr_in *addr =
-	      malloc(sizeof(struct scalanative_sockaddr_in));
+	      //	      malloc(sizeof(struct scalanative_sockaddr_in));
+	      calloc(1, sizeof(struct scalanative_sockaddr_in));
 
 	    // scalanative_convert_scalanative_sockaddr_in(
 	    //   (struct sockaddr_in *)in->ai_addr, addr, &size);
@@ -177,7 +160,6 @@ void scalanative_convert_addrinfo_X3(struct addrinfo *in,
     } else {
         out->ai_canonname = strdup(in->ai_canonname);
     }
-
     if (in->ai_next == NULL) {
         out->ai_next = NULL;
     } else {
@@ -193,7 +175,9 @@ void scalanative_convert_addrinfo(struct addrinfo *in,
   printf("---= My scalanative_convert_addrinfo: Begin\n");
 
   printf(
-    "---- My scalanative_convert_addrinfo: X3 using Magic 2021-05-31 11:52 -0400\n");
+	 "---- My scalanative_convert_addrinfo: X3 using Magic |%s|\n",
+	 "2021-05-31 12:20 -0400");
+
   scalanative_convert_addrinfo_X3(in, out);
 
   printf("---= MY scalanative_convert_addrinfo: End\n\n");
